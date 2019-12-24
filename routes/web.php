4e37b380f7af21e0db37971bfe6fa894065e73a1
Eko Simanjuntak <ecojuntak@gmail.com>
2019-12-24 12:47:34 +0700

    Add router and disable CSRF token verification

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,26 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return "Respon ini diterima dari path / dengan metode GET";
+});
+
+Route::get('/books', function(Request $request) {
+    return "Router ini nantinya akan digunakan untuk mengambil semua data buku. Closure method pada router ini juga menerima satu parameter yaitu $request yang digunakan untuk menampung query paramater";
+});
+
+Route::post('/books', function() {
+    return "Router ini nantinya akan digunakan untuk mencipatakan data buku yang baru";
+});
+
+Route::get('/books/{id}', function($id) {
+    return "Router ini nantinya akan digunakan untuk mengambil satu data buku dengan id=" . $id;
+});
+
+Route::put('/books/{id}', function($id) {
+    return "Router ini nantinya akan digunakan untuk mengubah data buku dengan id=" . $id;
+});
+
+Route::delete('/books/{id}', function($id) {
+    return "Router ini nantinya digunakan untuk menghapus data buku dengan id=" . $id;
 });
